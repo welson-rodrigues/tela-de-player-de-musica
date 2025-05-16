5a2ead247fbb22c0dfb23b29921d1bd9e072c94b
Aluno <aluno@debian.ifrn.local>
2025-05-16 15:06:55 -0300

    Imagem funcionando na tela

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,38 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { IconButton, MD3Colors } from 'react-native-paper';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      
+    <View style={styles.container}>
+      <Image style={styles.imagemMusica}
+      source={require("../assets/images/Oumou Sangaré.jpg")}
+      />
+      <View style={styles.botaoPlay}>
+        <IconButton
+        icon="play-circle"
+        iconColor={MD3Colors.error50}
+        size={68}
+        onPress={() => console.log('MERDA')}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    padding: 25,
+    width: "100%",
+  },
+
+  imagemMusica: {
+    marginTop: 50,
+    width: 340,
+    height: 340,
+    borderRadius: 25,
+  },
+  botaoPlay: {
+    
+  }
+})
