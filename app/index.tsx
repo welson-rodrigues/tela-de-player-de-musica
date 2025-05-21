e5200389074e5d2718a026d5cde248033e759605
Aluno <aluno@debian.ifrn.local>
2025-05-21 16:24:23 -0300

    Botão de Play adicionado

--- app/index.tsx
+++ app/index.tsx
@@ -7,12 +7,12 @@ export default function Index() {
       <Image style={styles.imagemMusica}
       source={require("../assets/images/Oumou Sangaré.jpg")}
       />
-      <View style={styles.botaoPlay}>
-        <IconButton
-        icon="play-circle"
-        iconColor={MD3Colors.error50}
-        size={68}
-        onPress={() => console.log('MERDA')}
+      <View style={styles.ellipseContainer}>
+        <Image style={styles.Ellipse}
+        source={require('../assets/images/Ellipse 1.png')}
+        />
+        <Image style={styles.Play}
+        source={require('../assets/images/Play.png')}
         />
       </View>
     </View>
@@ -22,17 +22,41 @@ export default function Index() {
 const styles = StyleSheet.create ({
   container: {
     flex: 1,
-    padding: 25,
+    padding: 30,
     width: "100%",
+    backgroundColor: "#A9A9A9"
   },
 
   imagemMusica: {
-    marginTop: 50,
-    width: 340,
-    height: 340,
+    marginTop: 10,
+    width: 330,
+    height: 330,
     borderRadius: 25,
   },
-  botaoPlay: {
-    
+
+  Ellipse: {
+    width: 68,
+    height: 68,
+    //marginTop: 50,
+    //  alignSelf: 'center',
+  },
+
+  Play: {
+    width: 30,
+    height: 30,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: [{ translateX: -15 }, { translateY: -15 }], // metade de 30 para centralizar
+  },
+
+  ellipseContainer: {
+    width: 68,
+    height: 68,
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginTop: 200,
   }
 })
