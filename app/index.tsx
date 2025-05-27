298612b858757fb52f9e5e515aef18368dacf537
Welson Rosendo Rodrigues <164583871+welson-rodrigues@users.noreply.github.com>
2025-05-27 04:45:08 +0000

    Barra de progresso adicionada

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,20 @@
 import { Image, StyleSheet, View } from "react-native";
-import { Icon, MD3Colors } from 'react-native-paper';
+import { Icon } from "react-native-paper";
 
 export default function Index() {
+  const progress = 60;  // Exemplo: 60% de progresso
+
   return (
     <View style={styles.container}>
       <View style={styles.menuContainer}>
         <Icon
-          source="menu-down"
+        source="menu-down"
           color={"#FFFFFF"}
           size={45}
         />
       </View>
 
-      <Image 
+      <Image
         style={styles.imagemMusica}
         source={require("../assets/images/Oumou Sangaré.jpg")}
       />
@@ -25,31 +27,33 @@ export default function Index() {
         />
       </View>
 
+      {/* Barra de Progresso */}
+      <View style={styles.progressContainer}>
+        <View style={[styles.progressBar, { width: `${progress}%` }]} />
+      </View>
+
       <View style={styles.controlsContainer}>
         <Icon
           source="repeat"
           color={"#FFFFFF"}
           size={40}
-          // style={styles.Menu}
         />
 
         <Icon
           source="skip-previous"
           color={"#FFFFFF"}
           size={40}
-          // style={styles.Repeat}
         />
 
         <View style={styles.ellipseContainer}>
-          <Image 
+          <Image
             style={styles.Ellipse}
-            source={require('../assets/images/Ellipse 1.png')}
+            source={require("../assets/images/Ellipse 1.png")}
           />
-
           <View style={styles.PlayContainer}>
             <Icon
-              source="play-outline"
-              color={'#FFFFFF'}
+            source="play-outline"
+              color={"#FFFFFF"}
               size={50}
             />
           </View>
@@ -59,7 +63,6 @@ export default function Index() {
           source="skip-next"
           color={"#FFFFFF"}
           size={40}
-          // style={styles.Next}
         />
 
         <Icon
@@ -77,7 +80,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 30,
     width: "100%",
-    backgroundColor: "black"
+    backgroundColor: "black",
   },
 
   imagemMusica: {
@@ -90,55 +93,57 @@ const styles = StyleSheet.create({
   Ellipse: {
     width: 80,
     height: 80,
-    //marginBottom: 200,
-    //  alignSelf: 'center',
-  },
-
-  Play: {
-    //width: 35,
-    //height: 30,
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: [{ translateX: -25 }, { translateY: -25 }], //metade de 30 para centralizar
   },
 
   ellipseContainer: {
     width: 80,
     height: 80,
-    position: 'relative',
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-    //marginTop: 50,
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
   },
 
   controlsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    marginTop: 120,
-    gap: 20, //espaço entre os ícones
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginTop: 30,
+    gap: 20,
   },
 
   PlayContainer: {
-    position: 'absolute',
-    top: '50%',
-    left: '55%',
+    position: "absolute",
+    top: "50%",
+    left: "55%",
     transform: [{ translateX: -25 }, { translateY: -25 }],
   },
 
   menuContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "flex-end",
     marginTop: -20,
   },
 
   starContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "flex-end",
     marginTop: 13,
+  },
+
+  progressContainer: {
+    width: "100%",
+    height: 5,
+    backgroundColor: "#555",
+    borderRadius: 5,
+    marginTop: 30,
+  },
+
+  progressBar: {
+    height: "100%",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 5,
   },
 });
